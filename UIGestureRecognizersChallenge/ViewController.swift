62f89fd79dffda0eb0bdad16eb494d7eef40921e
Lieb12 <lieberman03@gmail.com>
2020-10-16 09:49:42 -0400

    Initial Commit

---
+++ UIGestureRecognizersChallenge/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  UIGestureRecognizersChallenge
+//
+//  Copyright © 2020 MobileMakersEdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //MARK: - MVP
+    
+    
+    
+    //MARK: - Stretch #1
+    
+    
+    
+    //MARK: - Stretch #2
+   
+    
+    
+    //MARK: - Stretch #3
+    
+    
+    
+    //MARK: - Stretch #4
+    
+    
+    
+}
+
